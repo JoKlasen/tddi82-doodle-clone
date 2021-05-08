ff515704beb2085c63b0195cbb98b054df102629
osklu130 <osklu130@student.liu.se>
2021-05-08 16:48:25 +0200

    Ny branch

--- src/Menu_State.cc
+++ src/Menu_State.cc
@@ -2,6 +2,10 @@
 #include "State.h"
 
 sf::CircleShape shape(100.f);
+//titel
+//new game
+//continue
+//highscore
 
 Menu_State::Menu_State()
     : play{false}
@@ -15,7 +19,7 @@ void Menu_State::handle_event(sf::Event event)
 }
 void Menu_State::update()
 {
-   
+    
     shape.setFillColor(sf::Color::Green);
 }
 void Menu_State::render(sf::RenderTarget & target)
