b55e3654606e3a5484e7862623fa232b67f2846c
osklu130 <osklu130@student.liu.se>
2021-05-17 14:11:27 +0200

    börjat på power_up

--- include/Platform.h
+++ include/Platform.h
@@ -3,12 +3,16 @@
 
 #include "Entity.h"
 #include <SFML/Graphics.hpp>
+#include "Manager.h"
 
 
 class Platform : public Entity
 {
-public:
 
+
+
+public:
+   
     Platform();
     Platform( float, float );   //position 
     Platform( sf::Vector2f );   //position 
@@ -19,10 +23,48 @@ public:
     void update() override;
 
 private:
+   
+
+    class Power_Up: public Entity
+    {
+        
+    public:
+
+        Power_Up(int k)
+            : sprite{}, active{true}
+        {
+            sprite.setTexture(Manager<sf::Texture>::load("./resources/images/Doodle_Jump.png"));
+            //sprite.setPosition();
+        }
+
+        void render( sf::RenderTarget & target) 
+        {
+            target.draw(sprite);
+        }
+        void handle_collision( Entity const& )
+        {
+            if(active)
+            {
+                 // gör något med spelaren
+            }
+           // set actrive false
+           active = false; 
+        }
+        void update()
+        {
+
+        }
+
+    private:
+        sf::Sprite sprite;
+        bool active;
+    };
+
+
     void default_shape();
-
-
     sf::RectangleShape shape{ sf::Vector2f(75, 20) };
+    //Platform::Power_Up power_up;
+   
 
 };
 
