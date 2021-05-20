5144eb39eb6f711a337d0d8ff4ffd2fa7170c210
johkl473 <johkl473@student.liu.se>
2021-05-20 20:35:00 +0200

    Har lagt till stöd för ljud och ett enkelt studs-ljud, men inte implementerat mer än så. Finns lite ljudfiler att välja på om man vill utöka, annars får man leta själv.

--- src/Sounds.cc
+++ src/Sounds.cc
@@ -1,5 +1,4 @@
 #include "Sounds.h"
-#include <iostream>
 
 sf::Sound Sounds::m_jump{};
 
@@ -8,5 +7,4 @@ void Sounds::jump ()
     Sounds::m_jump.setBuffer(Sound_Manager::load(jump_file)); 
 
     m_jump.play();
-    //std::cout << "beep" << std::endl; //debugg
 }
